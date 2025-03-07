f65e7a91e8feed1815ab24ce8e43044c38da6463
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-03-07 12:59:58 +0100

    la vega document format

---
+++ lavega_document_format/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "La Vega Document Format",
+    "summary": """La Vega Document Format""",
+    "version": "18.0.1.0.0",
+    "description": """La Vega Document Format""",
+    "company": "Xtendoo",
+    "author": "Manuel Calero",
+    "website": "http://www.xtendoo.es",
+    "category": "Website",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/invoice/report_invoice_document.xml",
+    ],
+    "installable": True,
+    'application': False,
+}
